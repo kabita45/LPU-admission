f1bdefd1b7590513f39d6d1fa6bbb8275775af7c
Kabita maurya <kavimaurya535@gmail.com>
2024-02-20 13:51:28 +0530

    first commit

---
+++ user/footer.php
@@ -0,0 +1,12 @@
+<style type="text/css">
+	.style1 {
+		color: #000000;
+		font-weight: bold;
+	}
+</style>
+<div class="footer text-center">
+	<div class="style1">
+		<p> All Rights Reserved. &copy; 2023 Designed By : Saurabh Kumar, Master's in Computer Application,
+			Phagwara(Punjab) </p>
+	</div>
+</div>
